91119b6b6dc0ee9fe9996632269fa449810148cf
agachux <83849024+agachux@users.noreply.github.com>
2023-12-21 04:30:34 -0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_devonf-user
-add_lunch_combo omni_devonf-userdebug
-add_lunch_combo omni_devonf-eng
+add_lunch_combo twrp_devonf-user
+add_lunch_combo twrp_devonf-userdebug
+add_lunch_combo twrp_devonf-eng
+
+# vendor_boot-as-recovery
+	if [ "$TWRP_VENDOR_BOOT_RECOVERY" = "1" ]; then
+	   export TWRP_RESET_SETTINGS="disabled"
+	   export TWRP_VARIANT="vBaR"
+	fi
