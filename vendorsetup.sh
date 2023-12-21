4531bc3d58a4682365a239ebde5f1f70f44bc3d1
agachux <83849024+agachux@users.noreply.github.com>
2023-12-21 05:26:30 -0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,7 +10,7 @@ add_lunch_combo twrp_devonf-userdebug
 add_lunch_combo twrp_devonf-eng
 
 # vendor_boot-as-recovery
-	if [ "$TWRP_VENDOR_BOOT_RECOVERY" = "1" ]; then
-	   export TWRP_RESET_SETTINGS="disabled"
-	   export TWRP_VARIANT="vBaR"
+	if [ "$OF_VENDOR_BOOT_RECOVERY" = "1" ]; then
+	   export FOX_RESET_SETTINGS="disabled"
+	   export FOX_VARIANT="vBaR"
 	fi
